87ea289a736b2ab984649c708793622b03a89073
Ghxst <45855847+Ghxst@users.noreply.github.com>
2025-07-22 07:42:22 +0200

    add insecure launch option

--- launch.py
+++ launch.py
@@ -1,3 +1,4 @@
+import argparse
 import subprocess
 import tempfile
 
@@ -6,14 +7,56 @@ from pathlib import Path
 p = Path(__file__).resolve().parent.joinpath("extension")
 t = tempfile.mkdtemp()
 
-subprocess.Popen(
-    [
-        "chrome",
+def build_feats(insecure=False):
+    feats = [
+        "DisableLoadExtensionCommandLineSwitch"
+    ]
+
+    if insecure:
+        feats += [
+            "IsolateOrigins",
+            "site-per-process",
+            "BlockInsecurePrivateNetworkRequests",
+            "CrossSiteDocumentBlockingAlways",
+            "OutOfBlinkCors",
+            "WebSecurity",
+            "CORB",
+        ]
+
+    return feats
+
+def build_flags(insecure=False):
+    feats = build_feats(insecure)
+
+    flags = [
         f"--user-data-dir={t}",
         f"--load-extension={p}",
-        "--disable-features=DisableLoadExtensionCommandLineSwitch",
         "--no-first-run",
-    ],
+        f"--disable-features={','.join(feats)}"
+    ]
+
+    if insecure:
+        flags += [
+            "--disable-web-security",
+            "--disable-site-isolation-trials",
+            "--allow-running-insecure-content",
+            "--allow-insecure-localhost",
+            "--disable-client-side-phishing-detection",
+        ]
+
+    return flags
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--insecure",
+    action="store_true"
+)
+
+subprocess.Popen(
+    ["chrome"] + build_flags(
+        parser.parse_args().insecure,
+    ),
     stdout=subprocess.DEVNULL,
     stderr=subprocess.DEVNULL,
 )
